68d63c611522ffbe3e9d471e438ec5aa45b234f3
Pierre Mavro <pmavro@4tech.io>
2018-09-14 15:12:40 +0200

    feat: adding jq

--- prerequisites.sh
+++ prerequisites.sh
@@ -3,7 +3,7 @@
 # Install prerequisite for Docker and yq
 apt-get update
 apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common \
-                   bats golang
+                   bats golang jq
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
